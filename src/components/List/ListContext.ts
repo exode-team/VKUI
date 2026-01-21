9e7db5f188d4fc186da8e943afd8ce93de2696fd
aslanorlov <aslanorlov@gmail.com>
2026-01-21 18:57:56 +0700

    Release build 4.39.55

--- src/components/List/ListContext.ts
+++ src/components/List/ListContext.ts
@@ -2,5 +2,6 @@ import * as React from "react";
 import { noop } from "../../lib/utils";
 
 export const ListContext = React.createContext({
+  isDragging: false as boolean,
   toggleDrag: noop as (value: boolean) => void,
 });
